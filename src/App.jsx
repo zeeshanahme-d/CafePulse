49f3aa35dab1887ccf63d7dc56adda3f692a2ac8
zeeshanahme-d <ka265923@gmail.com>
2024-01-18 01:07:38 +0500

    ...

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Product from "./components/Product";
+import Review from "./components/Review";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Menu />
+      <About />
+      <Product />
+      <Review />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
